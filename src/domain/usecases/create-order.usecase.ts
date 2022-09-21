9deaac76ec044d282e469d3c6617e68429907d8d
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-21 18:38:14 -0300

    feat: add CreateOrderUseCase

---
+++ src/domain/usecases/create-order.usecase.ts
@@ -0,0 +1,14 @@
+import { Order, Product } from "@/domain/entities";
+
+export interface OrderProduct extends Pick<Product, "id" | "name" | "price"> {
+  quantity: number;
+}
+
+export interface CreateOrderParams {
+  id: string;
+  products: OrderProduct[];
+}
+
+export interface CreateOrderUseCase {
+  exec: (params: CreateOrderParams) => Order;
+}
