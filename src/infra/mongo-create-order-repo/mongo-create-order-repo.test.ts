1c4a96a9d44a10491f57e4a24e649b7cf965bc3a
Felipe Rocha <felipemota.rocha@gmail.com>
2022-09-21 23:16:14 -0300

    test: ensure MongoCreateOrderRepo creates an Order

---
+++ src/infra/mongo-create-order-repo/mongo-create-order-repo.test.ts
@@ -0,0 +1,24 @@
+import { mockCreateOrderParams, mockOrder } from "@/domain/test/mock-order";
+import { MongoHelper } from "../helpers/mongo-helper";
+import { MongoCreateOrderRepo } from "./mongo-create-order-repo";
+
+describe("MongoCreateOrderRepo", () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    await MongoHelper.connect(process.env.MONGO_URL!);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it("should create an Order", async () => {
+    const sut = new MongoCreateOrderRepo();
+
+    const params = mockCreateOrderParams();
+
+    const result = await sut.create(params);
+
+    expect(result).toEqual(mockOrder());
+  });
+});
